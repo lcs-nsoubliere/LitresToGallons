1ee972c20fc28527a0e60a68660a40d71eccbe01
Noah Soubliere <nsoubliere@lcs.on.ca>
2021-12-11 10:10:39 -0500

    Initial Commit

---
+++ LitresToGallons/LitresToGallonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LitresToGallonsApp.swift
+//  LitresToGallons
+//
+//  Created by Noah Alexandre Soubliere on 2021-12-11.
+//
+
+import SwiftUI
+
+@main
+struct LitresToGallonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
