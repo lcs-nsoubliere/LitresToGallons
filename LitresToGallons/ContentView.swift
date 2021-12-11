dd3f6f1f4bad47e1857e4cef3324f077c3c21128
Noah Soubliere <nsoubliere@lcs.on.ca>
2021-12-11 10:47:35 -0500

    Got the layout and math working, it currently has a slider from ranges from 1-100

--- LitresToGallons/ContentView.swift
+++ LitresToGallons/ContentView.swift
@@ -8,14 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    @State var litres = 1.0
+    
+    // MARK: Computed properties
+    //Process (the logic) goes here
+    var gallons: Double {
+        return litres * 0.265
+    }
+    
+    // "body" property is only for input and output
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            
+            HStack {
+                Text("Litres")
+                    .bold()
+                
+                Spacer()
+            }
+            
+            // Input: Measurment in Litres
+            Slider(value: $litres,
+                   in: 1.0...100.0,
+                   step: 0.1,
+                   label: {
+                Text("Litres:")
+            },
+                   minimumValueLabel: {
+                Text("1")
+            },
+                   maximumValueLabel: {
+                Text("100")
+            })
+            
+            // Output: Measurment in Litres
+            Text("\(String(format: "%.2f", litres)) L")
+                .bold()
+                .padding()
+            
+            HStack {
+                Text("Gallons:")
+                    .bold()
+                
+                Spacer()
+            }
+            
+            // Output: Measurment in Gallons
+            Text("\(String(format: "%.2f", gallons)) Gal")
+                .bold()
+                .padding()
+            
+            
+            Spacer()
+            
+        }
+        .padding()
+        .navigationTitle("Litres To Gallon")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
+
+
